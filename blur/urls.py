f656f62296e845c6f2eb2af404d419feea7d2bb7
saeedrz96 <saeedramezani75@gmail.com>
2023-07-26 12:29:25 +0330

    update: defined db, drf, cors-headers and static roots in settings

--- blur/urls.py
+++ blur/urls.py
@@ -6,4 +6,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('user_app.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
